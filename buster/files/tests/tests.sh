fc9a3f483bd44739df33d4ad55b1eba20da6e690
Frederico Martins <fredericoscm@gmail.com>
2019-10-15 13:29:07 +0100

    support posix and non-posix 'echo' implementations

--- buster/files/tests/tests.sh
+++ buster/files/tests/tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Shell script to test the Debian Docker image.
 #
@@ -14,40 +14,40 @@
 # Variables
 DEBIAN_FRONTEND=noninteractive
 
-/bin/echo "=== Docker Build Test ==="
+echo "=== Docker Build Test ==="
 
-/bin/echo -n "[TEST] Check if apt command is installed... "
-dpkg -s apt &>/dev/null
-if [[ "$?" -eq "0" ]]; then
-  /bin/echo 'OK'
+echo -n "[TEST] Check if apt command is installed... "
+dpkg -s apt > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+  echo 'OK'
 else
-  /bin/echo 'Failed'
+  echo 'Failed'
   exit 1
 fi
 
-/bin/echo -n "[TEST] Check package installation with apt... "
-apt-get -qq update && apt-get -y -qq install less &>/dev/null && less --help &>/dev/null
-if [[ "$?" -eq "0" ]]; then
-  /bin/echo 'OK'
+echo -n "[TEST] Check package installation with apt... "
+apt-get -qq update && apt-get -y -qq install less > /dev/null 2>&1 && less --version > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+  echo 'OK'
 else
-  /bin/echo 'Failed'
+  echo 'Failed'
   exit 2
 fi
 
-/bin/echo -n "[TEST] Check if debootstrap dir was removed... "
-if [[ ! -e /debootstrap ]]; then
-  /bin/echo 'OK'
+echo -n "[TEST] Check if debootstrap dir was removed... "
+if [ ! -e /debootstrap ]; then
+  echo 'OK'
 else
-  /bin/echo 'Failed'
+  echo 'Failed'
   exit 3
 fi
 
-/bin/echo -n "[TEST] Check if packages are correctly installed... "
+echo -n "[TEST] Check if packages are correctly installed... "
 MISSING=$(apt-get -qq -s install $(dpkg-query -W -f "\${Package} ") | wc -l)
-if [[ "${MISSING}" -eq "0" ]]; then
-  /bin/echo 'OK'
+if [ "${MISSING}" -eq "0" ]; then
+  echo 'OK'
 else
-  /bin/echo 'Failed'
+  echo 'Failed'
   exit 4
 fi
 
